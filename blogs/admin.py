9cd990c3720e8d6d1514b63876208a78e4dc859c
JinaboCV <jinabocv@gmail.com>
2025-04-15 08:15:22 +0100

    fetch categories

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from .models import Article, Category, Writer, Tag
 
-admin.site.register(Article)
+class AdminArticle(admin.ModelAdmin):
+    list_display = ('title', 'category', 'author')
+
+admin.site.register(Article, AdminArticle)
 admin.site.register(Writer)
 admin.site.register(Category)
 admin.site.register(Tag)
